aeab5b54cb919a5de717d791a917577b7aac3dc4
Alex <aliaksandr.lisavets@4dfun.io>
2023-03-20 15:43:05 +0100

    Frame hight btns

---
+++ Assets/Main/Scripts/ApplicationCore/RealtimeModels/LevelData.cs
@@ -0,0 +1,9 @@
+using Normal.Realtime;
+
+namespace Main.Scripts.ApplicationCore.RealtimeModels
+{
+    public class LevelData : RealtimeComponent<LevelDataModel>
+    {
+       
+    }
+}
--- Assets/Main/Scripts/VR/UI/FramePivot.cs
+++ Assets/Main/Scripts/VR/UI/FramePivot.cs
@@ -4,6 +4,21 @@ namespace Main.Scripts.VR.UI
 {
     public class FramePivot : MonoBehaviour
     {
-   
+        [SerializeField] private Transform levelButtons;
+        
+        public void PressBtnUp()
+        {
+            Debug.Log("PressUpBtn");
+        }
+        
+        public void PressBtnDown()
+        {
+            Debug.Log("PressDownBtn");
+        }
+
+        private void Update()
+        {
+            if (Camera.main != null) levelButtons.LookAt(Camera.main.transform.position);
+        }
     }
 }
